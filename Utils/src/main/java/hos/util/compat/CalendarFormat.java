c9edd1e2041dc95d166601fea80053ac158dc729
cjfsss <qfly163smx@163.com>
2022-04-28 23:58:55 +0800

    添加日期格式

--- Utils/src/main/java/hos/util/compat/CalendarFormat.java
+++ Utils/src/main/java/hos/util/compat/CalendarFormat.java
@@ -15,6 +15,7 @@ public interface CalendarFormat {
     String y_mo_d_h_m = "yyyy-MM-dd HH:mm";
     String y_mo_d = "yyyy-MM-dd";
     String y_mo = "yyyy-MM";
+    String y = "yyyy";
     String mo_d = "MM-dd";
     String mo_d_h_m = "MM-dd HH:mm";
     String h_m_s = "HH:mm:ss";
@@ -24,6 +25,7 @@ public interface CalendarFormat {
     String zh_y_mo_d_h_m = "yyyy年MM月dd日 HH时mm分";
     String zh_y_mo_d = "yyyy年MM月dd日";
     String zh_y_mo = "yyyy年MM月";
+    String zh_y = "yyyy年";
     String zh_mo_d = "MM月dd日";
     String zh_mo_d_h_m = "MM月dd日 HH时mm分";
     String zh_h_m_s = "HH时mm分ss秒";
